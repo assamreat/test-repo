63ce84d4192e34fd31923d0e0c9e45086d4e04aa
Bidyut Deka <bidyutranjan464@gmail.com>
2022-09-16 12:15:50 +0530

    added changes for deployment

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const path = require('path');
+// const path = require('path');
 // connect DB
 const sequelize = require('./util/database');
 
@@ -80,6 +80,20 @@ Appeal.hasOne(AppealDoc);
 
 RevertedAppeal.belongsTo(Appeal, { constraints: true, onDelete: 'CASCADE' });
 Appeal.hasOne(RevertedAppeal);
+
+if (process.env.NODE_ENV === 'production') {
+    // Express will serve up production assets
+    // like our main.js file, or main.css file
+    app.use(express.static('client/build'));
+
+    // Express will serve up the index.html file
+    // if it doesn't recognise the route
+
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 sequelize
     // .sync({ force: true })
